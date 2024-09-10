3d4be35e3b35d0f9925dbc1a2e25faee9ebf5f44
love-yuri <yuri2078170658@gmail.com>
2024-09-10 23:30:56 +0800

    feat: 添加 roles 属性到 UserInfo 接口

--- packages/types/src/user.d.ts
+++ packages/types/src/user.d.ts
@@ -2,10 +2,7 @@ import type { BasicUserInfo } from '@vben-core/typings';
 
 /** 用户信息 */
 interface UserInfo extends BasicUserInfo {
-  /**
-   * accessToken
-   */
-  token?: string;
+  roles?: string[];
 }
 
 export type { UserInfo };
